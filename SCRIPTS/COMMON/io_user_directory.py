a87594e1f3e01ea086f368871c7ffa3c705cf0de
muthumurugan <muthumurugan.ramalingam@hirepro.in>
2024-07-30 12:21:25 +0530

    Changed directory structure to accomodate diffrent os path

--- SCRIPTS/COMMON/io_user_directory.py
+++ SCRIPTS/COMMON/io_user_directory.py
@@ -1,6 +1,6 @@
 # This is the local user directory, should be ignored while pushing to git repo
 from pathlib import Path
 path = str(Path(__file__).parents[2])
-input_common_dir = path + r'\PythonWorkingScripts_InputData'
-output_common_dir = path + r'\PythonWorkingScripts_Output'
+input_common_dir = path + r'/PythonWorkingScripts_InputData'
+output_common_dir = path + r'/PythonWorkingScripts_Output'
 chrome_driver_dir = path
