82e8bdc8c0325b5afb777809a4adea049370f173
muthumurugan <muthumurugan.ramalingam@hirepro.in>
2024-12-30 16:54:19 +0530

    Email flow

--- SCRIPTS/COMMON/parser.py
+++ SCRIPTS/COMMON/parser.py
@@ -5,7 +5,6 @@ import html
 
 def extract_string(starts_with, string):
     # extract the links between starts_with and ends with < tag.
-    # pattern = r"{0}\s*([\w\d]+)(?=<)".format(starts_with)
     pattern = r"{0}\s*(\S+)(?=<)".format(starts_with)
     extracted_string = re.search(pattern, string)
     decoded_string = None
@@ -33,11 +32,55 @@ def extract_from_address(email_content):
     return email_content['From']
 
 
+def extract_cc_address(email_content):
+    if email_content['cc']:
+        return email_content['cc']
+    else:
+        return "EMPTY"
+
+
 def extract_mail_subject(email_content):
     return email_content['Subject']
 
 
 def extract_mail_date(email_content):
     return email_content['Date']
+
+
+def get_mailids_from_string(str):
+    lst = re.findall('\S+@\S+', str)
+    mail_ids = [e.strip("<>,") for e in lst]
+    return mail_ids
+
+
+def attachment_availability(email_content):
+    if email_content.is_multipart():
+        attachment = 'Available'
+    else:
+        attachment = 'Not Available'
+    return attachment
+
+
+def get_details_from_email_body(email_message):
+    if email_message.get_content_maintype() != 'multipart':
+        print(email_message.get_content_maintype())
+    for part in email_message.walk():
+        content_type = part.get_content_type()
+        if content_type == 'text/plain':
+            text = part.get_payload(decode=True).decode('utf-8')
+            text = re.sub(r'(<http.*>)|(Not\s+Mentioned)', '', text)
+        if content_type == 'text/html':
+            payload = part.get_payload(decode=True).decode('utf-8')
+            soup = BeautifulSoup(payload, "html.parser")
+            text = soup.get_text(strip=True)
+    return text
+
+
+def get_html_payload(email_message):
+    for part in email_message.walk():
+        content_type = part.get_content_type()
+        if content_type == 'text/plain' or content_type == 'text/html':
+            payload = part.get_payload()
+    return payload
 
 
